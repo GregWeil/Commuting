cc0f8a97e99bc2fcb2fbd6ae331a437fbf1a979c
Greg Weil <bailinbone15@gmail.com>
2022-04-04 20:47:28 -0400

    Cleaner tire marks at the start of the level

--- Commuting/Assets/TireMarks.cs
+++ Commuting/Assets/TireMarks.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TireMarks : MonoBehaviour {
   public float minDistance;
+  public float startDistance;
+  public float delay;
 
   private int turn;
   private LineRenderer line;
@@ -12,13 +14,14 @@ public class TireMarks : MonoBehaviour {
   void Start() {
     line = GetComponent<LineRenderer>();
     timing = FindObjectOfType<TimingController>();
-    line.SetPositions(new[] { transform.position, transform.position });
+    line.SetPositions(new[] { transform.TransformPoint(Vector3.up * startDistance), transform.position });
   }
 
   void Update() {
+    delay -= timing.GetTurnDt();
     Vector3 lastPosition = line.GetPosition(line.positionCount - 2);
     if (Vector3.Distance(lastPosition, transform.position) > minDistance) {
-      line.positionCount += 1;
+      if (delay < 0f) line.positionCount += 1;
     }
     line.SetPosition(line.positionCount - 1, transform.position);
   }
